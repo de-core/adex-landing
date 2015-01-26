0f0e37cf8f5d88acc0da9b964a92b7f35595c02a
alexander <ksiusha5@gmail.com>
2015-01-26 23:35:12 +0200

    map-fix

--- js/all.js
+++ js/all.js
@@ -1,51 +1,21 @@
 $(document).ready(function(){
 
-// Fixed menu
-// $(function () {
-//     $(window).scroll(function(event) {
-//         if($(this).scrollTop() > 150) {
-//         $("body").addClass('fixed')
-//     }
-//     else {
-//         $("body").removeClass('fixed')
-//     }
-//     });
-// });
-
 // // Open hidden part
-//   $('.search-switch .link').click(function(){
-//     $('.adv-search').toggleClass('hidden');
-//     $('.search-switch .link').toggleClass('hidden');
-//   });
-
-//   $('.title-block .link-open').click(function(){
-//   	$(this).toggleClass('minus');
-//   	$('.category').toggleClass('hidden');
-//   })
-
-//   $('.review-item .link-open').click(function(){
-//   	$(this).parent().find('.link-open').toggleClass('minus');
-//   	$(this).parent().find('.link-open').toggleClass('hidden');
-//   	$(this).parent().parent().find('table .tr-hidden').toggleClass('hidden');
-//   })
-
-// Select styling
-  // $('select').selectbox();
+  $('.reviews button').click(function(){
+    if ($(this).hasClass('reviews-open')){
+      $('.reviews .reviews-hidden.hidden').first().removeClass('hidden');
+      if (!$('.reviews .reviews-hidden').hasClass('hidden')){
+        $('.reviews .reviews-open').removeClass('reviews-open').addClass('reviews-close').text('Свернуть');
+      }
+    } else {
+      console.log('click')
+      $('.reviews .reviews-close').removeClass('reviews-close').addClass('reviews-open').text('Посмотреть еще отзывы');
+      $('.reviews .reviews-hidden').addClass('hidden');
+      $(window).scrollTop(1050);
+    }
+  });
 
 // Datepicker
-// $('.input-range-group .input-group-addon img').click(function(){
-//   $(this).parent().prev().datepicker('show');
-// });
-// var h=0;
-// $('.reviews .review-body').each(function(){
-
-//   var h_review=$(this).outerHeight();
-//   if (h< h_review){
-//     $('.reviews .review-body').css('height', h_review);
-//     h=h_review;
-//   }
-//   console.log(h)
-// })
 $('.input-range-group').each(function(){
   var start = $(this).find('.date-in');
   var end = $(this).find('.date-out');
@@ -65,49 +35,23 @@ $('.input-range-group').each(function(){
     $('.datepicker .selected').eq(1).css({background: 'red'});
   });
 });
-// $('.date').datepicker({
-//   language:'ru',
-//   autoclose: 'true',
-//   format: 'dd/mm/yyyy',
-//   orientation: 'top left'
-// });
+
 // Inputmask
   $('input[data-input="mobile"]').inputmask("+9(999)999-99-99");
 
-// Footable
-  // $('.footable').footable({
-  //   breakpoints: {
-  //     tablet: 600
-  //   }
-  // });
+// Yandex Map
+  ymaps.ready(init);
+  var myMap;
 
-// Contacts show and hide
-  // $('.contacts-link').click(function(){
-  //   if ( $('#block').css('display') == 'none' ) {
-  //  $('#block').animate({height: 'show'}, 1000);
-  //   $('.contacts-link img').attr('src','img/base/opened.png');
-  // } else {
-  // $('#block').animate({height: 'hide'}, 1000);
-  //  $('.contacts-link img').attr('src','img/base/closed.png');
-  // }
-  // });
- 
-  // $('.main').on('DOMMouseScroll mousewheel', function (e) {
-  // if(e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
-  //   //scroll down
-  //   if ( $('#block').css('display') == 'none' ) {
-  //     $('#block').animate({height: 'show'}, 1000);
-  //   }
-  //   $('.contacts-link img').attr('src','img/base/opened.png');
-  // } else {
-  //   //scroll up
-  //    if ( $('#block').css('display') == 'block' ) {
-  //  $('#block').animate({height: 'hide'}, 1000);
-  //   }
-  //    $('.contacts-link img').attr('src','img/base/closed.png');
-  // }
-  // //prevent page fom scrolling
-  // return false;
-  // });
+  function init(){     
+    myMap = new ymaps.Map("map", {
+        center: [55.76, 37.90],
+        zoom: 11
+    });
 
+    myPlacemark = new ymaps.Placemark([55.76, 37.64], { content: 'Москва!' });
+    myMap.geoObjects.add(myPlacemark);
+    myMap.behaviors.disable('scrollZoom');
+    myMap.behaviors.disable('multiTouch');
+  }
 });
